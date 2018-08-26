253db918bc5ec13ce21e30d45999f3e44fcad4f9
GTsude <40798678+GTsude@users.noreply.github.com>
2018-08-26 13:43:23 +0200

    Add files via upload

---
+++ Domain/Domain/Persistence/PersistenceCode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Domain.Business;
+using System.Globalization;
+using System.Data;
+
+namespace Domain.Persistence
+{
+    public class PersistenceCode
+    {
+        string _connectionString;
+
+        public PersistenceCode()
+        {
+            _connectionString = "server=localhost; port = 3306; user id=root; persistsecurityinfo = true; database = CliniresearchDB; password = Ratava989";
+        }
+
+        #region Get
+
+
+
+
+
+
+
+        #endregion
+
+
+    }
